59d1e9b8e05afad55e50cb9239e760d172ace2a2
문지은 <mjieun0956@gmail.com>
2024-04-02 01:04:11 +0900

    Chore: Tailwind Custom Color Setting

---
+++ src/app/home/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div>
+      <p className="text-primary">Primary</p>
+      <p className="text-secondary">Secondary</p>
+      <p className="text-success">Success</p>
+      <p className="text-danger">Danger</p>
+      <p className="text-warning">Warning</p>
+      <p className="text-info">Info</p>
+      <p className="text-light">Light</p>
+      <p className="text-dark">Dark</p>
+    </div>
+  );
+}
+
+export default HomePage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,49 @@
 /** @type {import('tailwindcss').Config} */
+
+import { toneMap } from "@nextcss/color-tools";
+
+function generateColors(color) {
+  return {
+    DEFAULT: color,
+    ...toneMap(color),
+  };
+}
+
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          ...generateColors("#007bff"),
+        },
+        danger: {
+          ...generateColors("#dc3545"),
+        },
+        secondary: {
+          ...generateColors("#6c757d"),
+        },
+        success: {
+          ...generateColors("#28a745"),
+        },
+        warning: {
+          ...generateColors("#ffc107"),
+        },
+        info: {
+          ...generateColors("#17a2b8"),
+        },
+        light: {
+          ...generateColors("#f8f9fa"),
+        },
+        dark: {
+          ...generateColors("#343a40"),
+        },
       },
     },
   },
   plugins: [],
-}
+};
