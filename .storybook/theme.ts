c057c170d82a560b54065d60f0da2ae81d594dbd
문지은 <mjieun0956@gmail.com>
2024-04-03 23:15:53 +0900

    feat: theme setting

---
+++ .storybook/theme.ts
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "light",
+  brandTitle: "Jieun",
+  brandUrl: "https://github.com/ji-eun-moon",
+  brandImage: "/mjieun.png",
+  brandTarget: "_blank",
+});
